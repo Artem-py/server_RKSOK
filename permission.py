92fd2b19f78c815ccd113a4a69c3ff393e6f3a2a
Artem Chervontsev <artyom.chervontsev@gmail.com>
2022-07-31 16:17:10 +0300

    Refactoring ver 1.6

--- permission.py
+++ permission.py
@@ -5,38 +5,30 @@ import asyncio
 from dataclasses import dataclass
 
 from request import Request
-from exceptions import SpecialOrgansServerError, DisconnectionError
+from exceptions import SpecialOrgansServerError, ConnectionError
 from request import get_message
 
 
 ENCODING = 'UTF-8'
 SPECIAL_ORGANS_SERVER_ADDRESS = ('vragi-vezde.to.digital', 51624)
+CHECK_REQUEST_HEADLINE = 'АМОЖНА? РКСОК/1.0\r\n'
 
 
 async def get_permission(request: Request) -> str:
     """Sends request to the Special Organs Server and returns its response
-    as Permission object
-    
-    """
-    special_organs_response = await connect_special_organs(request.full_text)
-    return special_organs_response
-
-
-async def connect_special_organs(message: str) -> str:
-    """Connects to the Special Organs Server in order to get permission to process client's message.
-    Returns string format message if server responses, otherwise raises SpecialOrgansServerError Error
+    in string format
     
     """
     try:
-        check_request_headline = 'АМОЖНА? РКСОК/1.0\r\n'
-        message_to_check = check_request_headline + message
+        message_to_check = CHECK_REQUEST_HEADLINE + request.full_text
         reader, writer = await asyncio.open_connection(*SPECIAL_ORGANS_SERVER_ADDRESS)
         writer.write(message_to_check.encode(ENCODING))
-        response = await asyncio.wait_for(get_message(reader), timeout=30)
+        special_organs_response = await asyncio.wait_for(get_message(reader), timeout=30)
         writer.close()
-        return response
-    except (asyncio.TimeoutError, DisconnectionError):
-        raise SpecialOrgansServerError('Failed to connect special organs server')
+        return special_organs_response
+    except ConnectionError:
+        raise SpecialOrgansServerError('Failed to connect to the Special Organs server')
+
 
 
     
